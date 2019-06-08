6099b98818197f5a664b123bc34535495dfed47b
Mohammad Hossein Karami <mhkarami1997@gmail.com>
2019-06-08 22:43:10 +0430

    change one hot encoding

--- AlibabaDNN/AlibabaDNN.py
+++ AlibabaDNN/AlibabaDNN.py
@@ -5,6 +5,7 @@ import pandas as pd
 import tensorflow as tf
 from copy import deepcopy
 from tensorflow import keras
+from sklearn import preprocessing
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OneHotEncoder
 from tensorflow.python.keras.models import Sequential
@@ -27,7 +28,7 @@ def ReadFile(status):
 
 		return newData,newData.groupby(['Log_Date', 'FROM', 'TO']).size()
 	else:
-		data = pd.read_csv('Data/test.csv', sep=',', low_memory=0, usecols=[1, 2, 3, 4, 5, 7], nrows=10000)
+		data = pd.read_csv('Data/test.csv', sep=',', low_memory=0, usecols=[1, 2, 3, 4], nrows=10000)
 
 		newData = pd.DataFrame(data)
 		newData['newColumn'] = 0
@@ -35,7 +36,7 @@ def ReadFile(status):
 
 		witchFile = 'Data/test1.csv'
 
-		return newData,newData.groupby(['Log_Date', 'FROM', 'TO']).size()
+		return newData,newData
 
 def WriteToCSV(csv,row,column,value):
 	csv.at[row, column] = value
@@ -60,7 +61,7 @@ def preprocess(train_data,train_label_values):
         label = (td[0], td[2], td[3])
         train_label = np.append(train_label, dic[label])
         date = jdatetime.datetime.strptime(td[0], "%Y/%m/%d")
-
+        
         d = date.day
         m = date.strftime("%m")
         w = date.weekday()
@@ -80,7 +81,7 @@ def preprocess(train_data,train_label_values):
 
     # train_label = keras.utils.to_categorical(train_label_values, 1)
     # test_label = keras.utils.to_categorical(test_label, 10)
-    #train_label = train_label.astype('float32')  
+    #train_label = train_label.astype('float32')
     #temp = temp.astype('float32')
 
     return temp, train_label
@@ -113,20 +114,121 @@ def train_model(model):
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
 
-def OneHotEncoding(input):
-	values = array(input)
-	label_encoder = LabelEncoder()
-	integer_encoded = label_encoder.fit_transform(values)
-	print(integer_encoded)
-	# binary encode
-	onehot_encoder = OneHotEncoder(sparse=False)
-	integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
-	onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
-	print(onehot_encoded)
-	# invert first example
-	inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
-	print(inverted)
+def OneHotEncoding(data,dataTest):
+    # Discrete column to be one-hot-encoded
+    #col = 'FROM'
+    #train = pd.DataFrame(data)
+    #test = pd.DataFrame(dataTest)
+    ## Create dummy variables for each level of `col`
+    #train_animal_dummies = pd.get_dummies(data[col], prefix=col)
+    #train = train.join(train_animal_dummies)
 
+    #test_animal_dummies = pd.get_dummies(dataTest[col], prefix=col)
+    #test = test.join(test_animal_dummies)
+
+    ## Find the difference in columns between the two datasets
+    ## This will work in trivial case, but if you want to limit to just one
+    ## feature
+    ## use this: f = lambda c: col in c; feature_difference = set(filter(f,
+    ## train)) - set(filter(f, test))
+    #feature_difference = set(train) - set(test)
+
+    ## create zero-filled matrix where the rows are equal to the number
+    ## of row in `test` and columns equal the number of categories missing
+    ## (i.e.  set difference
+    ## between relevant `train` and `test` columns
+    #feature_difference_df = pd.DataFrame(data=np.zeros((test.shape[0],
+    #len(feature_difference))),
+    #                                     columns=list(feature_difference))
+
+    ## add "missing" features back to `test
+    #test = test.join(feature_difference_df)
+
+    df = pd.DataFrame(data)
+
+    one_hot_from = pd.get_dummies(df['FROM'])
+    one_hot_to = pd.get_dummies(df['TO'])
+
+    te = []
+    te1 = []
+    te2 = []
+    te3 = []
+    te4 = []
+    i = 0
+    for tds in data.values:
+        date = jdatetime.datetime.strptime(tds[0], "%Y/%m/%d")
+        
+        d = date.day
+        m = date.strftime("%m")
+        w = date.weekday()
+        s = (int(m) % 12 + 3) // 3
+
+        mytemp = [d,int(m),w,s]
+        te.append(mytemp)
+
+        te1.append(d)
+        te2.append(int(m))
+        te3.append(w)
+        te4.append(s)
+
+        i += 1
+
+       
+
+    #df = df.drop('FROM',axis = 1)
+    #df = df.join(one_hot)
+
+    one_hot_d = pd.get_dummies(te1)
+    one_hot_m = pd.get_dummies(te2)
+    one_hot_w = pd.get_dummies(te3)
+    one_hot_s = pd.get_dummies(te4)
+
+    arrz = [one_hot_d ,one_hot_m , one_hot_w , one_hot_s]
+    one_hot_date = np.array(arrz)
+
+    print(one_hot_date)
+    print('-------------------')
+    print(one_hot_from)
+
+    #test.head(1)
+    ## limit to categorical data using df.select_dtypes()
+    #test = test.select_dtypes(include=[object])
+    #test.head(1)
+    #le = preprocessing.LabelEncoder()
+
+
+    ## 2/3.  FIT AND TRANSFORM
+    ## use df.apply() to apply le.fit_transform to all columns
+    #X_2 = test.apply(le.fit_transform)
+    #X_2.head()
+    ## 1.  INSTANTIATE
+    #enc = preprocessing.OneHotEncoder()
+
+    ## 2.  FIT
+    #enc.fit(X_2)
+
+    ## 3.  Transform
+    #onehotlabels = enc.transform(X_2).toarray()
+    #onehotlabels.shape
+
+    ## as you can see, you've the same number of rows 891
+    ## but now you've so many more columns due to how we changed all the
+    ## categorical data into numerical data
+
+    #print(onehotlabels)
+
+    #values = np.array(input)
+    #label_encoder = LabelEncoder()
+    #integer_encoded = label_encoder.fit_transform(values)
+    #print(integer_encoded)
+    ## binary encode
+    #onehot_encoder = OneHotEncoder(sparse=False)
+    #integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    #onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    #print(onehot_encoded)
+    ## invert first example
+    #inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+    #print(inverted)
 def Draw(train_data,model):
 	for x in range(5000, 6000):
 		test_data = train_data[x,:].reshape((9,))
@@ -136,23 +238,25 @@ def Draw(train_data,model):
             # plt.title('Prediction: %d Label: %d' % (predicted_number, label))
             # plt.imshow(test_data, cmap=plt.get_cmap('gray_r'))
             # plt.show()
-
 if __name__ == '__main__':	
-	status = 1
-	data,train_label = ReadFile(status)
+    status = 1
+    data,train_label = ReadFile(status)
+    dataTest,train_labelTest = ReadFile(0)
 
-	train_data, train_label = preprocess(data,train_label)
+    OneHotEncoding(data,dataTest)
 
-	model = create_model()
+    train_data, train_label = preprocess(data,train_label)
 
-	train_model(model)
+    model = create_model()
 
-	Draw(train_data,model)
+    train_model(model)
 
-	#test
-	#WriteToCSV(data,1,'newColumn',2)
-	#test
+    Draw(train_data,model)
 
-	#day, month, dayOfWeekNum, monthOfYearNum = DateBreaker(data)
+    #test
+    #WriteToCSV(data,1,'newColumn',2)
+    #test
 
-	print('finish')
+    #day, month, dayOfWeekNum, monthOfYearNum = DateBreaker(data)
+
+    print('finish')
